5d28b1b0ef18bd5419605d4bb78a616db83041a5
LFGdlfgOEDJGoeJ <f.g.tesmer@mailbox.org>
2025-01-09 17:19:06 +0100

    kommentare

--- Spielfeldfunktionen.h
+++ Spielfeldfunktionen.h
@@ -6,14 +6,23 @@
 using namespace std;
 
 const int fg = 4;
-
+/**
+* @brief Ein neues Spielfeld wird auf die Konsole geschrieben mit unterschiedlichen Farben (Florian)
+* @param[in] spielfeld[fg][fg] zweidimensionales 4x4 Array
+**/
 void spielfelderstellen(string spielfeld[fg][fg]); /**zeichnet das neuste Spielfeld auf die Konsole (F)**/
 
-void spielfeldrotierenUhr(string spielfeld[fg][fg]);        /**rotiert die Platten auf dem Spielfeld im Uhrzeigersinn,
-                                                            jedoch ohne die Positionskoordinaten zu ver�ndern (F)**/
+/**
+* @brief Das Spielfeld wird im Uhrzeigersinn rotiert (Florian)
+* @param[in] spielfeld[fg][fg] zweidimensionales 4x4 Array
+**/
+void spielfeldrotierenUhr(string spielfeld[fg][fg]);        
 
-void spielfeldrotierenGegenUhr(string spielfeld[fg][fg]);   /**rotiert die Steine auf dem Spielfeld gegen den Uhrzeigerssinn,
-                                                            jedoch ohne die Positionskoordinaten zu ver�ndern (F)**/
+/**
+* @brief Das Spielfeld wird gegen den Uhrzeigersinn rotiert (Florian)
+* @param[in] spielfeld[fg][fg] zweidimensionales 4x4 Array
+**/
+void spielfeldrotierenGegenUhr(string spielfeld[fg][fg]);  
 
 int checkSiegquer(const string spielfeld[fg][fg]);        /**�berpr�ft, ob ein Spieler horizontal gewonnen hat (F)**/
 
@@ -29,13 +38,30 @@ void spielzug1(string spielfeld[fg][fg], string &spieler1, string &spieler2, con
 void spielzug2(string spielfeld[fg][fg], string &spieler1, string &spieler2, const string spielfeldarray[fg][fg], bool checksiegStatus, int winstreak, bool Spieler1istDran);  /** spielt den Spielzug von spieler 2, wenn Name von Spieler 2
                                                             computer ist, spielt der computer - SpielzugComputer()**/
 
-void SpielzugComputer(string spielfeld[fg][fg]);      /** f�hrt (zuf�lligen) Spielzug von Computer aus **/
+/**
+* @brief Der Spielzug des Computers wird per Zufall ausgeführt (Florian)
+* @param[in] spielfeld[fg][fg] zweidimensionales 4x4 Array
+**/
+void SpielzugComputer(string spielfeld[fg][fg]); 
 
-void SpielzugComputerL(string spielfeld[fg][fg]);      /** f�hrt (logischen) Spielzug von Computer aus **/
+/**
+* @brief Der Spielzug des Computers wird einen Schritt vorausschauend ausgeführt (Florian)
+* @param[in] spielfeld[fg][fg] zweidimensionales 4x4 Array
+**/
+void SpielzugComputerL(string spielfeld[fg][fg]); 
 
-void funny_spielfelderstellen(string spielfeld[fg][fg]);    /** gibt das Spielfeld ohne Zeilenumbruch aus (F)**/
+/**
+* @brief Ein neues Spielfeld horizontal auf die Konsole geschrieben (Florian)
+* @param[in] spielfeld[fg][fg] zweidimensionales 4x4 Array
+**/
+void funny_spielfelderstellen(string spielfeld[fg][fg]); 
 
-void steinVerschieben(string spielfeld[fg][fg], string spieler, int spielernummer); /** optionaler Spielzug, orthogonale
-                                                                                    Verschiebung des gegnerischen Steins(F)**/
+/**
+* @brief Der optionale Spielzug wird nach Auswahl ausgeführt (Florian)
+* @param[in] spielfeld[fg][fg] zweidimensionales 4x4 Array
+* @param[in] spieler Spielername
+* @param[in] spielernummer Spieler 1 oder 2, um XX und YY zu unterscheiden
+**/
+void steinVerschieben(string spielfeld[fg][fg], string spieler, int spielernummer); 
 
 #endif //SPIELFELDFUNKTIONEN_H
